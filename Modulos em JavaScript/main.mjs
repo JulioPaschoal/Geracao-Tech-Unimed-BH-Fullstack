5bcb7a53e43947f3c5dc5663ac351435a23dfab4
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2022-07-03 14:36:24 -0300

    Geração Tech Unimed-BH DIO

---
+++ Modulos em JavaScript/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('JulioPaschoal', 36));
+console.log(mostraCidade('JulioPaschoal', 'Alto Piquiri'));
+console.log(mostraHobby('JulioPaschoal',    'Desenvolver'));
---
+++ Variaveis e Tipos/atividade-pratica/index.js
@@ -0,0 +1,39 @@
+/* 
+function verficaPalindromo(string) {
+    if (!string) return "string inexistente";
+
+    return string.split("").reverse().join("") === string;
+}
+
+console.log(verficaPalindromo("gato")); */
+
+
+
+/* function verficaPalindromo2(string) {
+    if (!string) return "string inexistente";
+
+    for (let i = 0; i < string.length / 2; i++) {
+        if (string[i] !== string[string.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(verficaPalindromo2("gato")); */
+
+function substituiPares(array) {
+    for (let i = 0; i, array.length; i++) {
+        if(array[i] === 0){
+            console.log("Você já é zero!");
+        }else if (array[i] % 2 === 0){
+            console.log(` Subistituindo ${array[i]} por 0...`);
+            array[i] = 0;
+        }
+    }
+
+    return array;
+}
+
+let arr = [1, 3, 4, 6, 80, 33, 23, 90];
+console.log(substituiPares(arr));
