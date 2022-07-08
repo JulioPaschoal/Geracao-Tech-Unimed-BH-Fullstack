4d6757331fc882541eea1b00a562a9787f0a355d
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2022-07-08 00:03:36 -0300

    Fnalizando projeto em Angular

--- Introdução ao Angular/course-manager/src/app/app.component.ts
+++ Introdução ao Angular/course-manager/src/app/app.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'course-manager';
 
-  name: string = 'Julio Paschoal'
+
+  name: string = 'John';
 }
--- Introdução ao Angular/course-manager/src/app/app.module.ts
+++ Introdução ao Angular/course-manager/src/app/app.module.ts
@@ -1,40 +1,32 @@
-import { ReplacePipe } from './pipe/replace.pape';
-import { CourseListComponent } from './courses/course-list.component';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
-import { StarComponent } from './star/star.component';
-import { NavBarComponent } from './nav-bar/nav-bar.componet';
-import { RouterModule } from '@angular/router';
-import { Error404Compoennt } from './error-404/error-404.component';
+import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
+import { Error404Compoennt } from './core/component/error-404/error-404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
-    NavBarComponent,
     Error404Compoennt
-
-
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {
-        path:'', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: 'courses', component: CourseListComponent 
-      },
-      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      }, {
         path: '**', component: Error404Compoennt
       }
-    ])
+    ]),
+    CourseModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
