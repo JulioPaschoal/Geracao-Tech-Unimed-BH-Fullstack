219c163d88ceb7ddbfc3daeddd66cf632c5e99be
JULIO PASCHOAL <juliocpaschoal@gmail.com>
2022-07-18 11:03:46 -0300

    Cadastrando Usuarios

--- Crud-Angular/src/app/pages/users/user-form/user-form.component.ts
+++ Crud-Angular/src/app/pages/users/user-form/user-form.component.ts
@@ -1,5 +1,7 @@
+import { UserService } from './../../../services/user.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-user-form',
@@ -8,8 +10,9 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class UserFormComponent implements OnInit {
   userForm: FormGroup;
+  users: Array<User> = [];
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private userService: UserService) {
     this.userForm = this.fb.group({
       id: 0,
       nome: '',
@@ -20,11 +23,20 @@ export class UserFormComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
+    this.getUsers();
   }
 
-  createUser(){
-    
+  getUsers() {
+    this.userService.getUsers().subscribe(response => {
+      this.users = response;
+    })
+  }
+
+  createUser() {
+    this.userForm.get('id')?.patchValue(this.users.length + 1);
+    this.userService.postUser(this.userForm.value).subscribe(result => {
+      console.log(`Usuario ${result.nome} ${result.sobrenome} cadastrado com sucesso!`);
+    });
   }
 
 
--- Crud-Angular/src/app/services/user.service.ts
+++ Crud-Angular/src/app/services/user.service.ts
@@ -1,22 +1,32 @@
-import { HttpClient } from '@angular/common/http';
+import { User } from './../models/user';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   apiUrl = 'https://sheet.best/api/sheets/044fd310-3710-4679-8079-4c81475dc336';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'content-Type': 'application/json'
+    })
+  }
 
   constructor(private httpClient: HttpClient) { }
 
   // C.R.U.D - CREATE, READ, UPDATE, DELETE \\
 
   // READ RETORNA A LISTA DE USUARIOS \\
-  getUsers():Observable<User[]> {
+  getUsers(): Observable<User[]> {
     return this.httpClient.get<User[]>(this.apiUrl);
   }
 
-  
+  // CREATE SALVA USUARIO NO BANCO \\
+  postUser(user: User): Observable<User> {
+    return this.httpClient.post<User>(this.apiUrl, user, this.httpOptions);
+  }
+
+
 }
